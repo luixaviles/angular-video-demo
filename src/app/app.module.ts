9d6c27a4d449671c9c405fe6c656c9aeb3dc000e
luixaviles <luis.avilesz@gmail.com>
2017-06-16 16:24:47 -0400

    Adding basic video player and single control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { VgCoreModule } from "videogular2/core";
+import { VgControlsModule } from "videogular2/controls";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgCoreModule,
+    VgControlsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
